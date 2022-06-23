5b1ff5876ae0a8ae4ef2e8d59b5785a22d5a3ddc
ifeomafine <102186386+ifeomafine@users.noreply.github.com>
2022-06-23 20:53:03 +0100

    overlays & project boxes ready

--- script.js
+++ script.js
@@ -11,14 +11,48 @@ function createBubble() {
 
     setTimeout(() => {
         createElement.remove()
-    } , 4000)
+    } , 10000)
 }
 
 setInterval(createBubble , 100)
 
+// DARK/LIGHT MODE TOGGLE
 
+const toggle = document.getElementById('toggle');
 
+toggle.addEventListener('click' , myFunction);
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
 
+ // MODAL JAVASCRIPT
+
+let modal = document.getElementById("myModal"); // Get the modal
+let btn = document.getElementById("myBtn"); // Get the button that opens the modal
+let span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
+
+// FOR RESPONSIVE NAV-BAR
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('nav')[0];
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+    });
 
 
 /*document.addEventListener('mousemove', function(e) {
